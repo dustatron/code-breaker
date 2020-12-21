50ffc8fa2a98cc2e1f368eaee8653202023b2b87
Dusty McCord <Dustymccord@gmail.com>
2020-12-20 17:27:26 -0800

    add two lists and swapping lists.

---
+++ src/components/ColorList.js
@@ -0,0 +1,34 @@
+let ColorList = [
+  {
+    id: "100",
+    name: "Red",
+    hex: "#ff0000",
+  },
+  {
+    id: "102",
+    name: "Green",
+    hex: " #47d147",
+  },
+  {
+    id: "103",
+    name: "Blue",
+    hex: "#0099ff",
+  },
+  {
+    id: "104",
+    name: "Yellow",
+    hex: "#ffff00",
+  },
+  {
+    id: "105",
+    name: "White",
+    hex: "#f2f2f2",
+  },
+  {
+    id: "106",
+    name: "Black",
+    hex: "#1a1a1a",
+  },
+];
+
+export default ColorList;
--- src/components/Dragable/Dragable.js
+++ src/components/Dragable/Dragable.js
@@ -1,48 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import ColorList from "../ColorList";
 
-function Dragable(props) {
-  let list = [
-    {
-      id: "100",
-      name: 1,
-    },
-    {
-      id: "102",
-      name: 2,
-    },
-    {
-      id: "103",
-      name: 3,
-    },
-    {
-      id: "104",
-      name: 4,
-    },
-  ];
+function Dragable() {
+  const [currentList, updateList] = useState(ColorList);
+  const [selectedList, updateSelected] = useState([]);
+
+  const getList = (listName) => {
+    if (listName === "list1") {
+      return currentList;
+    }
+    if (listName === "list2") {
+      return selectedList;
+    }
+    return console.error("No list found");
+  };
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+  };
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    const start = result.source.index;
+    const end = result.destination.index;
+
+    if (source.droppableId === destination.droppableId) {
+      // Reorder list
+      const newOrder = reorder(getList(source.droppableId), start, end);
+
+      // Update the correct list
+      if (source.droppableId === "list2") {
+        updateSelected(newOrder);
+      } else {
+        updateList(newOrder);
+      }
+    } else {
+      // Move to other list
+      const moveListItems = move(getList(source.droppableId), getList(destination.droppableId), source, destination);
+
+      updateList(moveListItems.list1);
+      updateSelected(moveListItems.list2);
+    }
+  };
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="characters">
-        {(provided) => {
-          return (
-            <ul ref={provided.innerRef} {...provided.droppableProps} className="characters">
-              {list.map(({ id, name }, index) => {
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(provided) => (
-                      <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                        {name}
-                      </li>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </ul>
-          );
-        }}
-      </Droppable>
-    </DragDropContext>
+    <>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <h2>List</h2>
+        <Droppable droppableId="list1">
+          {(provided) => {
+            return (
+              <ul ref={provided.innerRef} {...provided.droppableProps} className="characters">
+                {currentList.map(({ id, name, hex }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="color-box">
+                          {name}
+                          <div className="color-box-square" style={{ backgroundColor: hex }}></div>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            );
+          }}
+        </Droppable>
+        <h2>Selected</h2>
+        <Droppable droppableId="list2">
+          {(provided) => {
+            return (
+              <ul ref={provided.innerRef} {...provided.droppableProps} className="characters">
+                {selectedList.map(({ id, name, hex }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="color-box">
+                          {name}
+                          <div className="color-box-square" style={{ backgroundColor: hex }}></div>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+    </>
   );
 }
 
