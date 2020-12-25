dabb92faae7237b9b32d71379d99f3e66faabe7c
Dusty McCord <Dustymccord@gmail.com>
2020-12-25 15:31:09 -0800

    address linting errors.

--- src/components/MasterCode/MasterCode.js
+++ src/components/MasterCode/MasterCode.js
@@ -1,97 +1,104 @@
-import React, { useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import { generateCode } from './CodeHelper';
-import { v4 } from 'uuid';
+import React, { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { generateCode } from "./CodeHelper";
+import { v4 } from "uuid";
 
 // Context
-import { useMasterCode } from '../../context/GameContext';
-import { useMasterCodeUpdate } from '../../context/GameContext';
+import { useMasterCode } from "../../context/GameContext";
+import { useMasterCodeUpdate } from "../../context/GameContext";
 
-import { useWin } from '../../context/GameContext';
-import { useWinUpdate } from '../../context/GameContext';
+import { useWin } from "../../context/GameContext";
+import { useWinUpdate } from "../../context/GameContext";
 
-import { useGameStarted } from '../../context/GameContext';
-import { useGameStartedUpdate } from '../../context/GameContext';
+import { useGameStarted } from "../../context/GameContext";
+import { useGameStartedUpdate } from "../../context/GameContext";
 
-import { useTurnList } from '../../context/TurnContext';
-import { useTurnListUpdate } from '../../context/TurnContext';
+import { useTurnList } from "../../context/TurnContext";
+import { useTurnListUpdate } from "../../context/TurnContext";
 
-import ColorListData from '../../data/ColorListData';
+import ColorListData from "../../data/ColorListData";
 
-function MasterCode () {
-    // Context
-    const masterCode = useMasterCode();
-    const masterCodeUpdate = useMasterCodeUpdate();
+function MasterCode() {
+  // Context
+  const masterCode = useMasterCode();
+  const masterCodeUpdate = useMasterCodeUpdate();
 
-    const gameStarted = useGameStarted();
-    const gameStartedUpdate = useGameStartedUpdate();
+  const gameStarted = useGameStarted();
+  const gameStartedUpdate = useGameStartedUpdate();
 
-    const winState = useWin();
-    const winStateUpdate = useWinUpdate();
+  const winState = useWin();
+  const winStateUpdate = useWinUpdate();
 
-    const turnList = useTurnList();
-    const turnListUpdate = useTurnListUpdate();
+  const turnList = useTurnList();
+  const turnListUpdate = useTurnListUpdate();
 
-    useEffect(
-        () => {
-            if (winState) {
-                toast('YOU BROKE THE CODE');
-                handleEndGame();
-            }
-        },
-        [ winState ]
-    );
+  const notify = () => toast("Game Is Ready");
+  const endGame = () => toast("Game Ended");
 
-    const notify = () => toast('Game Is Ready');
-    const endGame = () => toast('Game Ended');
+  const handleStartGame = () => {
+    notify();
+    winStateUpdate(false);
+    gameStartedUpdate(true);
+    masterCodeUpdate(generateCode(ColorListData));
+    turnListUpdate([], true);
+    winStateUpdate(false);
+  };
 
-    const handleStartGame = () => {
-        notify();
-        winStateUpdate(false);
-        gameStartedUpdate(true);
-        masterCodeUpdate(generateCode(ColorListData));
-        turnListUpdate([], true);
-        winStateUpdate(false);
-    };
+  const handleEndGame = () => {
+    gameStartedUpdate(false);
+    endGame();
+  };
 
-    const handleEndGame = () => {
-        gameStartedUpdate(false);
-        endGame();
-    };
+  useEffect(() => {
+    if (winState) {
+      toast("YOU BROKE THE CODE");
+      // End the game
+      gameStartedUpdate(false);
+      endGame();
+    }
+  }, [gameStartedUpdate, winState]);
 
-    return (
-        <div className="top">
-            <Toaster />
-            <div className="top-code-row">
-                {!gameStarted && masterCode.length > 0 && <div className="top-code-row--title">Master Code:</div>}
-                {!gameStarted &&
-                masterCode.length === 0 && <div className="top-code-row--title"> Can You Break the Code? </div>}
-                {gameStarted && <div className="top-code-row--title"> {10 - turnList.length} Turns Left </div>}
-                <div className="top-code-row--color">
-                    {/* Game End & show master code  */}
-                    {!gameStarted &&
-                        masterCode.length > 0 &&
-                        masterCode.map((color) => (
-                            <div
-                                key={v4()}
-                                className="top-code-row--color-boxes"
-                                style={{ backgroundColor: color.hex }}
-                            />
-                        ))}
-                </div>
-            </div>
-            <div className="top-button-row">
-                <button className="top-button-row--btn" onClick={handleStartGame}>
-                    {gameStarted ? 'Restart Game' : 'Start Game'}
-                </button>
-                {gameStarted && (
-                    <button className="top-button-row--btn" onClick={handleEndGame}>
-                        End Game
-                    </button>
-                )}
-            </div>
+  return (
+    <div className="top">
+      <Toaster />
+      <div className="top-code-row">
+        {!gameStarted && masterCode.length > 0 && (
+          <div className="top-code-row--title">Master Code:</div>
+        )}
+        {!gameStarted && masterCode.length === 0 && (
+          <div className="top-code-row--title"> Can You Break the Code? </div>
+        )}
+        {gameStarted && (
+          <div className="top-code-row--title">
+            {" "}
+            {10 - turnList.length} Turns Left{" "}
+          </div>
+        )}
+        <div className="top-code-row--color">
+          {/* Game End & show master code  */}
+          {!gameStarted &&
+            masterCode.length > 0 &&
+            masterCode.map((color) => (
+              <div
+                key={v4()}
+                className="top-code-row--color-boxes"
+                style={{ backgroundColor: color.hex }}
+              />
+            ))}
         </div>
-    );
+      </div>
+      <div className="top-button-row">
+        <button className="top-button-row--btn" onClick={handleStartGame}>
+          {gameStarted ? "Restart Game" : "Start Game"}
+        </button>
+        {gameStarted && (
+          <button className="top-button-row--btn" onClick={handleEndGame}>
+            End Game
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MasterCode;
--- src/components/TakeTurn/TakeTurn.js
+++ src/components/TakeTurn/TakeTurn.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import { useSelectedList } from '../../context/ColorListContext';
-import { useSelectedListUpdate } from '../../context/ColorListContext';
+import React from "react";
+import { useSelectedList } from "../../context/ColorListContext";
+import { useSelectedListUpdate } from "../../context/ColorListContext";
 
-import { turnListContext, useTurnList } from '../../context/TurnContext';
-import { useTurnListUpdate } from '../../context/TurnContext';
+import { useTurnList } from "../../context/TurnContext";
+import { useTurnListUpdate } from "../../context/TurnContext";
 
-import { useMasterCode } from '../../context/GameContext';
-import { useCurrentListUpdate } from '../../context/ColorListContext';
+import { useCurrentListUpdate } from "../../context/ColorListContext";
 
-import { useGameStarted } from '../../context/GameContext';
-import { useGameStartedUpdate } from '../../context/GameContext';
+import { useGameStarted } from "../../context/GameContext";
+import { useGameStartedUpdate } from "../../context/GameContext";
 
-import { useWinUpdate } from '../../context/GameContext';
+import ColorListData from "../../data/ColorListData";
 
-import ColorListData from '../../data/ColorListData';
+function TakeTurn() {
+  const currentListUpdate = useCurrentListUpdate();
+  const turnList = useTurnList();
+  const selectedList = useSelectedList();
+  const updateSelectedList = useSelectedListUpdate();
+  const turnListUpdate = useTurnListUpdate();
 
-function TakeTurn () {
-    const masterCode = useMasterCode();
-    const currentListUpdate = useCurrentListUpdate();
-    const turnList = useTurnList();
-    const selectedList = useSelectedList();
-    const updateSelectedList = useSelectedListUpdate();
-    const turnListUpdate = useTurnListUpdate();
+  const gameStartedUpdate = useGameStartedUpdate();
+  const gameStarted = useGameStarted();
 
-    const gameStartedUpdate = useGameStartedUpdate();
-    const gameStarted = useGameStarted();
+  const handleTakeTurn = () => {
+    // Must have 4 colors selected to take turn
+    if (selectedList.length < 1) {
+      return;
+    }
+    turnListUpdate(selectedList);
+    updateSelectedList([]);
+    currentListUpdate(ColorListData);
 
-    const winUpdate = useWinUpdate();
+    if (turnList.length + 1 >= 10) {
+      gameStartedUpdate(false);
+    }
+  };
 
-    const handleTakeTurn = () => {
-        // Must have 4 colors selected to take turn
-        if (selectedList.length < 1) {
-            return;
-        }
-        turnListUpdate(selectedList);
-        updateSelectedList([]);
-        currentListUpdate(ColorListData);
-
-        if (turnList.length + 1 >= 10) {
-            gameStartedUpdate(false);
-        }
-    };
-
-    return (
-        <div>
-            <button
-                onClick={handleTakeTurn}
-                disabled={selectedList.length >= 1 && gameStarted ? false : true}
-                className="color-picker-btn"
-            >
-                {selectedList.length >= 1 ? 'Make Guess' : 'Add A Colors'}
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        onClick={handleTakeTurn}
+        disabled={selectedList.length >= 1 && gameStarted ? false : true}
+        className="color-picker-btn"
+      >
+        {selectedList.length >= 1 ? "Make Guess" : "Add A Colors"}
+      </button>
+    </div>
+  );
 }
 
 export default TakeTurn;
